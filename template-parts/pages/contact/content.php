b7191bebecb241368884fcd5d7f2338135c37b91
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-01-24 13:15:49 +0000

    FIX: Page header

---
+++ template-parts/pages/contact/content.php
@@ -0,0 +1,35 @@
+<section class="contact-section pt-40 pb-36">
+	<div class="theme-container theme-grid">
+
+		<div class="col-span-6">
+			<h2 class="mb-10">Wir helfen Ihnen gerne weiter</h2>
+			<p class="mb-12">Kontaktieren Sie uns für eine persönliche Beratung durch unsere Experten.</p>
+			<div class="grid grid-cols-6 md:gap-6 lg:gap-7">
+				<div class="col-span-3 box box--sm">
+					<h3 class="mb-4">Adresse</h3>
+					<p>
+					Gruber Pumpen und <br>
+					Systeme AG <br>
+					Hofackerstrasse 77 <br>
+					4132 Muttenz
+					</p>
+				</div>
+				<div class="col-span-3 box box--sm">
+					<h3 class="mb-4">Kontaktdaten</h3>
+					<a href="tel:">+41 61 511 80 18</a>
+					<a href="mailto:">info@gruber-pumpen.ch</a>
+				</div>
+				<div class="col-span-6 box box--sm">
+					<h3 class="mb-4">Öffnungszeiten</h3>
+					<p>
+						Montag bis Donnerstag: 07:30-17:00 Uhr <br>
+						Freitag: 07:30- 16:00 Uhr
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-start-8 col-span-5 box box--xl -ml-6">
+			<h3>Kontaktieren Sie uns</h3>
+		</div>
+	</div>
+</section>
--- template-parts/pages/page-header.php
+++ template-parts/pages/page-header.php
@@ -1,39 +1,16 @@
-<section class="page-header flex items-end bg-black text-blue-shade-0 min-h-[464px] mt-32 relative py-11 px-8 <?php
-$intro_image_id = get_field('page_header_image');
-if ($intro_image_id):
-    echo '';
-else:
-    echo 'pt-28 lg:pt-64 pb-20 lg:pb-40';
+<?php
+$intro_image_id = get_field( 'page_header_image' );
+if ( $intro_image_id ) :
+	?><section class="page-header page-header--background-image" style="background-image: url(<?php echo esc_url( wp_get_attachment_image_url( $intro_image_id, 'full' ) ); ?>);"><span class="absolute inset-0 bg-overlay"></span><span class="absolute inset-0 bg-overlay-2"></span><?php
+else :
+	?>
+	<section class="page-header">
+	<?php
 endif;
-?>" style="<?php
-if ($intro_image_id):
-    echo 'background-image: url(' . wp_get_attachment_image_url($intro_image_id, 'full') . '); background-size: cover; background-position: center;';
-endif;
-?>">
+?>
 
-    <!-- Overlay -->
-    <?php if ($intro_image_id): ?>
-        <div class="absolute inset-0 bg-overlay"></div>
-    <?php endif; ?>
-
-    <div class="relative">
-        <div class="flex flex-col w-full mx-24">
-            <div class="page-header--title max-w-[60%]  invisible fade-in--noscroll">
-                <?php
-                $page_header_title = get_field('page_header_title');
-                if ($page_header_title): ?>
-                    <h1 class="page-header--title py-4"><?php echo $page_header_title; ?></h1>
-                <?php endif; ?>
-            </div>
-
-            <div class="page-header--breadcrumbs invisible fade-in--noscroll">
-                <?php do_action('breadcrumbs'); ?>
-            </div>
-
-        </div>
-    </div>
-
-    <script>
-
-    </script>
+	<div class="theme-container relative z-10">
+		<h1 class="page-header--title py-4"><?php the_field( 'page_header_title' ) ?></h1>
+		<?php do_action( 'breadcrumbs' ); ?>
+	</div>
 </section>
