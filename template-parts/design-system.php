b99301d0d695cb11c9a8d6e16851995f505cdee2
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-01-27 13:33:26 +0000

    Added Design System

---
+++ template-parts/design-system.php
@@ -0,0 +1,90 @@
+<div class="theme-container">
+	<section class="py-16">
+		<h2 class="font-outfit font-semibold text-[2.75rem] text-blue-shade-4 leading-[3.375rem] text mb-8" style="leading-trim: both; text-edge: cap;">01.Colors</h2>
+		<hr>
+		<div class="flex gap-4 pt-8">
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-blue-shade-3 text-white py-4 px-8 mb-4">Theme Color 1</span>
+				#0D6394 <br> .bg-blue-shade-3
+			</div>
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-orange-shade-1 text-white py-4 px-8 mb-4">Theme Color 2</span>
+				#D3594A <br> .bg-orange-shade-1
+			</div>
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-blue-shade-4 text-white py-4 px-8 mb-4">Theme Color 3</span>
+				#063047 <br> .bg-blue-shade-3
+			</div>
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-blue-shade-1 text-blue-shade-4 py-4 px-8 mb-4">Theme Color 4</span>
+				#F0F7FA <br> .bg-blue-shade-1
+			</div>
+		</div>
+		<div class="flex gap-4 pt-8">
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-blue-shade-4 text-white py-4 px-8 mb-4">Titles</span>
+				#063047 <br> .bg-blue-shade-4
+			</div>
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-blue-shade-1 text-blue-shade-4 py-4 px-8 mb-4">Titles</span>
+				#F0F7FA <br> .bg-blue-shade-1
+			</div>
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-blue-shade-2 text-white py-4 px-8 mb-4">Body</span>
+				#688494 <br> .bg-blue-shade-2 
+			</div>
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-blue-shade-1 text-blue-shade-4 py-4 px-8 mb-4">Body</span>
+				#F0F7FA <br> .bg-blue-shade-1
+			</div>
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-orange-shade-1 text-white py-4 px-8 mb-4">Overlines</span>
+				#D3594A <br> .bg-orange-shade-1
+			</div>
+			<div class="inline-flex flex-col justify-center text-center">
+				<span class="bg-blue-shade-3 text-white py-4 px-8 mb-4">Icons</span>
+				#0D6394 <br> .bg-blue-shade-3
+			</div>
+		</div>
+	</section>
+	<section class="py-16">
+		<h2 class="font-outfit font-semibold text-[2.75rem] text-blue-shade-4 leading-[3.375rem] text mb-8" style="leading-trim: both; text-edge: cap;">02.Typography</h2>
+		<hr>
+		<div class="grid grid-cols-3 gap-8 mt-8">
+			<div class="col-span-3">
+				<h1 class="text-title mb-4">Heading - H1 .text-title </h1>
+				<h2 class="text-title-h2 mb-4">Heading - H2 .text-title-h2</h2>
+				<h3 class="text-title-h3 mb-4">Heading - H3 .text-title-h3</h3>
+				<h4 class="text-title-h4 mb-4">Heading - H4 .text-title-h4</h4>
+				<p class="text-subtitle mb-4">Subtitle .text-subtitle</p>
+				<p class="text-overline mb-4">Overline .text-overline</p>
+				<p class="text-p--1 mb-4">.text-p--1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe omnis alias, eius odio quo ut non impedit sapiente ipsa harum earum, expedita quis nihil accusamus ducimus sequi tempore cumque aut.</p>
+				<p class="text-p--2 mb-4">.text-p--2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe omnis alias, eius odio quo ut non impedit sapiente ipsa harum earum, expedita quis nihil accusamus ducimus sequi tempore cumque aut.</p>
+			</div>
+		</div>
+	</section>
+	<section class="py-16">
+		<h2 class="font-outfit font-semibold text-[2.75rem] text-blue-shade-4 leading-[3.375rem] text mb-8" style="leading-trim: both; text-edge: cap;">03.Buttons</h2>
+		<hr>
+		<div class="grid grid-cols-5 gap-8 mt-8">
+			<div class="col flex flex-col justify-center items-center gap-y-3 p-8">
+				<a class="btn btn--primary" href="">button primary</a>
+				<span class="whitespace-nowrap break-keep">.btn .btn--primary</span>
+			</div>
+			<div class="col flex flex-col justify-center items-center gap-y-3 p-8">
+				<a class="btn btn--secondary" href="">button secondary</a>
+				<span class="whitespace-nowrap break-keep">.btn .btn--secondary</span>
+			</div>
+			<div class="col bg-blue-shade-3 flex flex-col justify-center items-center gap-y-3 p-8">
+				<a class="btn btn--transparent" href="">button transparent</a>
+				<span class="text-blue-shade-1 whitespace-nowrap break-keep">.btn .btn--transparent</span>
+			</div>
+			<div class="col bg-blue-shade-1 flex flex-col justify-center items-center gap-y-3 p-8">
+
+			</div>
+			<div class="col flex flex-col justify-center items-center gap-y-3 p-8">
+
+</div>
+		</div>
+	</section>
+</div>
