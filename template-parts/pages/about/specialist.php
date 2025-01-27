345f53c03e19bef6f82080d0f7994d4b9fb7bb14
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-01-27 18:55:49 +0000

    Added specialist section

---
+++ template-parts/pages/about/specialist.php
@@ -0,0 +1,18 @@
+<section class="section-specialists pb-28">
+	<div class="theme-container theme-grid">
+		<div class="col-span-2 md:col-span-6 order-2 xl:order-1">
+			<div class="flex flex-col xl:justify-between xl:h-full">
+				<h2 class="text-title-h3 text-blue-shade-4 mb-4 md:mb-12 xl:mb-0"><?php the_field( 'specialist_title' ); ?></h2>
+				<p class="text-p--2"><?php the_field( 'specialist_description' ); ?></p>
+			</div>
+		</div>
+		<div class="col-span-2 md:col-span-4 xl:col-span-5 xl:col-start-8 order-1 xl:order-2 mb-8 md:mb-16 xl:mb-0">
+			<?php
+			$image = get_field( 'specialist_image' );
+			if ( $image ) :
+				echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'max-w-full w-full h-auto object-cover rounded-[1.875rem] overlflow-hidden' ) );
+			endif;
+			?>
+		</div>
+	</div>
+</section>
--- template-parts/pages/about/why.php
+++
@@ -1,6 +0,0 @@
-<section class="section-why">
-	<div class="theme-container">
-		<h2></h2>
-		<h3></h3>
-	</div>
-</section>
