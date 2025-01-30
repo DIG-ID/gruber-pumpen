ccea4a203f8473952d34c6cf6faed9ce73ebc6c0
Daniel-Digid <daniel@dig.id>
2025-01-30 16:57:35 +0000

    Update products-overview.php

--- template-parts/components/products-overview.php
+++ template-parts/components/products-overview.php
@@ -35,7 +35,7 @@
             while ($products_query->have_posts()) :
                 $products_query->the_post();
                 ?>
-                <div class="col-span-1 md:col-span-3 xl:col-span-4 fade-in md:bg-white md:px-9 xl:px-7 pt-6 pb-10 md:pt-10 md:pb-16 rounded-3xl">
+                <div class="col-span-1 md:col-span-3 xl:col-span-4 fade-in md:bg-white md:px-9 xl:px-7 pt-6 pb-10 md:pt-10 md:pb-16 mb-10 xl:mb-0 rounded-3xl">
                     <div>
                     <?php
                     $image = get_post_thumbnail_id(); 
