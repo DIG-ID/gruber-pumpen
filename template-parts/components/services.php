2273c822ffa3ec1d5c82336ae1a9aafa7a862409
Daniel-Digid <daniel@dig.id>
2025-01-30 15:18:27 +0000

    d

--- template-parts/components/services.php
+++ template-parts/components/services.php
@@ -1,29 +1,63 @@
-<section class="section-services bg-blue-shade-3 relative overflow-hidden">
-	<h2 class="text-subtitle text-blue-shade-0 absolute top-24 left-1/2 -translate-x-1/2 z-20"><?php the_field( 'services_title', 'options' ); ?></h2>
-	<div class="services-teaser flex">
-		<?php
-		if ( have_rows( 'services_list', 'options' ) ) :
+<div class="theme-container theme-grid">
+	<!-- Services -->
+	<div class="col-span-2 md:col-span-6 xl:col-span-12 flex flex-col">
+		<div class="flex items-center gap-4 invisible fade-in">
+			<?php
+			$services_eyebrow = get_field('services_eyebrow');
+			if ($services_eyebrow): ?>
+				<h4 class="text-orange-shade-1 text-center text-eyebrow"><?php echo $services_eyebrow; ?></h4>
+			<?php endif; ?>
+			<hr class="border border-orange-shade-1 w-[4.5rem]" />
+		</div>
+		<div class="flex items-center justify-between invisible fade-in">
+			<?php
+			$services_title = get_field('services_title');
+			if ($services_title): ?>
+				<h2 class="text-title-h2 text-blue-shade-4 py-8 invisible fade-in max-w-[500px]">
+					<?php echo $services_title; ?>
+				</h2>
+			<?php endif; ?>
 
-			while ( have_rows( 'services_list', 'options' ) ) :
-				the_row();
-				$image = get_sub_field( 'image' );
-				if ( $image ) :
-					$image = wp_get_attachment_image_url( $image, 'full' );
-				else :
-					$image = 'https://picsum.photos/800/1000?random=1';
-				endif;
+			<?php
+			$services_link = get_field('services_link');
+			if ($services_link):
+				$services_link_target = $services_link['target'] ? $services_link['target'] : '_self';
 				?>
-
-				<?php ?>
-				<div class="service-box bg-no-repeat bg-center" style="background-image: url(<?php echo esc_url( $image ); ?>);">
-					<div class="z-10">
-						<h2 class="font-poppins font-normal text-3xl text-white tracking-[2.56px] uppercase text-center mb-8"><?php the_sub_field( 'title' ); ?></h2>
-						<p class="font-poppins font-normal text-base text-white leading-7 tracking-[0.96px] text-center"><?php the_sub_field( 'description' ); ?></p>
-					</div>
-				</div>
-				<?php
-			endwhile;
-		endif;
-		?>
+				<a class="btn btn--secondary uppercase inline-block" href="<?php echo esc_url($services_link['url']); ?>"
+					target="<?php echo esc_attr($services_link_target); ?>"><?php echo esc_html($services_link['title']); ?></a>
+			<?php endif; ?>
+		</div>
 	</div>
-</section>
+	<?php
+	$services_args = array(
+		'post_type'   => 'service',
+		'post_status' => 'publish',
+		'nopaging'    => true,
+		'orderby'     => 'date',
+		'order'       => 'ASC',
+	);
+
+	$services_query = new WP_Query($services_args);
+	if ($services_query->have_posts()) :
+		while ($services_query->have_posts()) :
+			$services_query->the_post();
+			?>
+			<div class="col-span-1 md:col-span-3 xl:col-span-3 fade-in md:bg-white md:px-9 xl:px-7 pt-6 pb-10 md:pt-10 md:pb-16 rounded-3xl">
+				<div class="min-h-[55px]">
+					<?php
+					$image = get_field('icon');
+					if ($image):
+						echo wp_get_attachment_image($image, 'full', false, array('class' => 'fade-in'));
+					endif;
+					?>
+				</div>
+				<h2 class="text-title-h4 text-blue-shade-4 text-left mt-3 md:mt-6 mb-5 md:mb-6 fade-in"><?php the_field('title'); ?>
+				</h2>
+				<p class="text-p--2 invisible fade-in"><?php the_field('text'); ?></p>
+			</div>
+		<?php
+		endwhile;
+		wp_reset_postdata();
+	endif;
+	?>
+</div>
--- template-parts/pages/home/services-and-brands.php
+++ template-parts/pages/home/services-and-brands.php
@@ -1,52 +1,10 @@
-<section class="section-page-services-and-brands py-8 overflow-hidden">
-	<div class="theme-container">
-
+<section class="section-page-services-and-brands py-8 xl:py-44 xl:mx-14 bg-[#F5F7FA] xl:rounded-[50px] overflow-hidden">
+	
 		<!-- Services and Products -->
-		<div
-			class="services-and-brands-container theme-grid relative flex flex-col gap-36 bg-blue-shade-0 py-44 rounded-[50px]">
-			<!-- Services -->
-			<div class="col-span-2 md:col-span-6 xl:col-span-12 flex flex-col gap-12">
-				<div class="flex items-center gap-4 invisible fade-in">
-					<hr class="border border-orange-shade-1 w-[4.5rem]" />
-					<?php
-					$services_eyebrow = get_field('services_eyebrow');
-					if ($services_eyebrow): ?>
-						<h4 class="text-orange-shade-1 text-center uppercase"><?php echo $services_eyebrow; ?></h4>
-					<?php endif; ?>
-					<hr class="border border-orange-shade-1  w-[4.5rem]" />
-				</div>
-				<?php
-				$services_title = get_field('services_title');
-				if ($services_title): ?>
-					<h2 class="text-title-h2 text-blue-shade-4 py-8 invisible fade-in max-w-[500px]">
-						<?php echo $services_title; ?>
-					</h2>
-				<?php endif; ?>
-
-				<?php
-				$services_types = get_field('services_types');
-				if (have_rows('services_types')): ?>
-					<div class="flex flex-wrap gap-8 justify-center">
-						<?php while (have_rows('services_types')):
-							the_row(); ?>
-							<div class="flex gap-4 fade-in bg-white p-6 max-w-[282px] rounded-3xl">
-								<div class="flex flex-col max-w-[415px]">
-									<?php
-									$icon = get_sub_field('title');
-									if ($icon):
-										echo theme_service_svg_icon($icon);
-									endif;
-									?>
-									<h2 class="text-title-h2 lg:text-2xl text-left fade-in"><?php the_sub_field('title'); ?>
-									</h2>
-									<p class="text-p1 py-4 invisible fade-in"><?php the_sub_field('description'); ?></p>
-								</div>
-							</div>
-						<?php endwhile; ?>
-					</div>
-				<?php endif; ?>
-			</div>
-
+		
+		<?php get_template_part( 'template-parts/components/services'); ?> 
+		
+		<div class="theme-container theme-grid relative flex flex-col gap-36 bg-blue-shade-0 xl:py-44 rounded-[50px]">
 			<!-- Products -->
 			<div class="col-span-2 md:col-span-6 xl:col-span-12 flex flex-col gap-12">
 				<?php
@@ -80,7 +38,8 @@
 					</div>
 				<?php endif; ?>
 			</div>
-
+		</div>
+		<div class="theme-container theme-grid relative flex flex-col gap-36 bg-blue-shade-0 xl:py-44 rounded-[50px]">
 
 			<!-- Brands -->
 			<div class="col-span-2 md:col-span-6 xl:col-span-12 flex flex-col items-center gap-12 pt-32">
@@ -129,7 +88,5 @@
 					</div>
 				<?php endif; ?>
 			</div>
-
 		</div>
-	</div>
 </section>
