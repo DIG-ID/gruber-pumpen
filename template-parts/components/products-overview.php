fa9578db28841e977c52538a287f2691644d5871
Daniel-Digid <daniel@dig.id>
2025-01-30 16:40:25 +0000

    d

--- template-parts/components/products-overview.php
+++ template-parts/components/products-overview.php
@@ -36,11 +36,11 @@
                 $products_query->the_post();
                 ?>
                 <div class="col-span-1 md:col-span-3 xl:col-span-4 fade-in md:bg-white md:px-9 xl:px-7 pt-6 pb-10 md:pt-10 md:pb-16 rounded-3xl">
-                    <div class="min-h-[55px]">
+                    <div>
                     <?php
                     $image = get_post_thumbnail_id(); 
                     if ($image):
-                        echo wp_get_attachment_image($image, 'full', false, array('class' => 'fade-in'));
+                        echo wp_get_attachment_image($image, 'full', false, array('class' => 'max-w-[294px] fade-in'));
                     endif;
                     ?>
 
