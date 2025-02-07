752812774094ba5d1e2573df40a21dee41fa477c
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-02-07 18:14:41 +0000

    fix google maps on functions

--- functions.php
+++ functions.php
@@ -120,7 +120,7 @@ function gruber_pumpen_theme_enqueue_styles() {
 
 	wp_script_add_data( 'font-awesome-kit', 'crossorigin', 'anonymous' );
 
-	if ( is_page_template( 'page-templates/page-home.php' ) || is_page_template( 'page-templates/page-arrival-contacts.php' ) || is_admin() ) :
+	if ( is_page_template( 'page-templates/page-contact.php' ) || is_admin() ) :
 		wp_enqueue_script( 'google-map-settings', get_stylesheet_directory_uri() . '/assets/js/google-maps.js', array( 'jquery' ), $theme_version, true );
 		wp_enqueue_script( 'google-map-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBAZN5TfX1aWmjodZ4e_6sOcaJV4D59jfo&callback=initMap', array(), $theme_version, true );
 	endif;
