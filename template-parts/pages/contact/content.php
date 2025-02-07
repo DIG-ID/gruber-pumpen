43eb74b8bccc7fd35dea3539193791006860f680
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-02-07 18:07:21 +0000

    contact form responsive, form and map

--- template-parts/pages/contact/content.php
+++ template-parts/pages/contact/content.php
@@ -1,42 +1,52 @@
 <section class="contact-section pt-40 pb-36">
 	<div class="theme-container theme-grid">
 
-		<div class="col-span-6">
-			<h2 class="text-title-h2 text-blue-shade-4 mb-10">Wir helfen Ihnen gerne weiter</h2>
-			<p class="text-p--2 mb-12">Kontaktieren Sie uns für eine persönliche Beratung durch unsere Experten.</p>
-			<div class="grid grid-cols-6 md:gap-6 lg:gap-7">
+		<div class="col-span-2 md:col-span-6 xl:col-span-6">
+			<h2 class="text-title-h2 text-blue-shade-4 mb-10"><?php the_field( 'intro_title' ); ?></h2>
+			<p class="text-p--2 mb-12"><?php the_field( 'intro_description' ); ?></p>
+			<div class="grid grid-cols-6 md:gap-6 gap-7">
 				<div class="col-span-3 box box--sm">
-					<h3 class="text-subtitle text-blue-shade-4 mb-2">Adresse</h3>
-					<p class="text-p--2">
-						Gruber Pumpen und <br>
-						Systeme AG <br>
-						Hofackerstrasse 77 <br>
-						4132 Muttenz
-					</p>
+					<h3 class="text-subtitle text-blue-shade-4 mb-2"><?php the_field( 'address_title' ); ?></h3>
+					<p class="text-p--2"><?php the_field( 'address_description' ); ?></p>
 				</div>
 				<div class="col-span-3 box box--sm">
-					<h3 class="text-subtitle text-blue-shade-4 mb-2">Kontaktdaten</h3>
+					<h3 class="text-subtitle text-blue-shade-4 mb-2"><?php the_field( 'contacts_title' ); ?></h3>
 					<div class="flex flex-col">
 						<a class="text-p--2 inline-block" href="tel:">+41 61 511 80 18</a>
 						<a class="text-p--2 inline-block underline" href="mailto:">info@gruber-pumpen.ch</a>
 					</div>
-
 				</div>
 				<div class="col-span-6 box box--sm">
-					<h3 class="text-subtitle text-blue-shade-4 mb-2">Öffnungszeiten</h3>
-					<p class="text-p--2 mb-4">
-						Montag bis Donnerstag: 07:30-17:00 Uhr <br>
-						Freitag: 07:30- 16:00 Uhr
-					</p>
+					<h3 class="text-subtitle text-blue-shade-4 mb-2"><?php the_field( 'hours_title' ); ?></h3>
+					<p class="text-p--2 mb-4"><?php the_field( 'hours_description' ); ?></p>
 				</div>
 				<div class="col-span-6">
-					<h3 class="text-title-h4 text-blue-shade-4 mb-2">Folgen Sie uns auf</h3>
+					<h3 class="text-title-h4 text-blue-shade-4 mb-2"><?php esc_html_e( 'Folgen Sie uns auf', 'gruber-pumpen' ) ?></h3>
 					<?php do_action( 'socials', 'light' ); ?>
 				</div>
 			</div>
 		</div>
-		<div class="col-start-8 col-span-5 box box--xl -ml-6">
-			<h3 class="text-title-h3 text-blue-shade-4">Kontaktieren Sie uns</h3>
+		<div class="col-span-2 md:col-span-5 xl:col-start-8 xl:col-span-5 box box--xl xl:-ml-6 mt-24 xl:mt-0">
+			<h3 class="text-title-h3 text-blue-shade-4 mb-4 xl:mb-10"><?php the_field( 'contact_form_title' ); ?></h3>
+			<?php
+			$form_sc = get_field( 'contact_form_form' );
+			if ( $form_sc ):
+				echo do_shortcode( $form_sc );
+			endif;
+			?>
 		</div>
 	</div>
+
+</section>
+<section class="map-wrapper">
+	<?php
+	$location = get_field( 'map' );
+	if ( $location ) :
+		?>
+		<div class="acf-map w-full h-[474px] xl:h-[714px]" data-zoom="16">
+			<div class="marker" data-lat="<?php echo esc_attr( $location['lat'] ); ?>" data-lng="<?php echo esc_attr( $location['lng'] ); ?>"></div>
+		</div>
+		<?php
+	endif;
+	?>
 </section>
