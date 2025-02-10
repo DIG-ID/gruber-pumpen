3965b7b465b7d7bdfe6e2656797750cdc0f6abbb
Daniel-Digid <daniel@dig.id>
2025-02-10 15:39:25 +0000

    d

--- template-parts/posts/flexible-steigleitung/layout.php
+++ template-parts/posts/flexible-steigleitung/layout.php
@@ -1,14 +1,14 @@
 <section class="section-content-1 pt-10 md:pt-20 xl:pt-24 pb-24 xl:pb-40 overflow-hidden bg-blue-shade-0">
     <div class="theme-container theme-grid">
-        <div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:mb-16 xl:pb-14">
+        <div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/content-1'); ?>
         </div>
         
-        <div id="content-2" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:mb-16 xl:pb-14">
+        <div id="content-2" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/content-2'); ?>
         </div>
         
-        <div id="content-3" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1">
+        <div id="content-3" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/content-3'); ?>
         </div>
         
@@ -16,7 +16,7 @@
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/gallery'); ?>
         </div>
         
-        <div id="sidebar" class="col-span-2 md:col-span-6 xl:col-span-4 col-start-1 xl:col-start-9 xl:row-start-1">
+        <div id="sidebar" class="col-span-2 md:col-span-6 xl:col-span-4 col-start-1 xl:col-start-9 xl:row-start-1 xl:row-span-2">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/sidebar'); ?>
         </div>
     </div>
--- template-parts/posts/flexible-steigleitung/sidebar.php
+++ template-parts/posts/flexible-steigleitung/sidebar.php
@@ -1,5 +1,15 @@
 <h4 class="text-title-h4 !text-base md:!text-3xl text-blue-shade-4 mb-9 md:mb-11"><?php the_field( 'sidebar_title' ); ?></h4>
 <h6 class="text-p--1 !text-blue-shade-4 mb-4 md:mb-7"><?php the_field( 'sidebar_subtitle_1' ); ?></h6>
-<p class="text-p--2 pb-9 md:pb-0 xl:pb-9"><?php the_field( 'sidebar_text_1' ); ?></p>
+<p class="text-p--2 pb-9 md:pb-0 xl:pb-9 xl:max-w-[320px]"><?php the_field( 'sidebar_text_1' ); ?></p>
 <h6 class="text-p--1 !text-blue-shade-4 mb-4 md:mb-7"><?php the_field( 'sidebar_subtitle_2' ); ?></h6>
-<p class="text-p--2"><?php the_field( 'sidebar_text_2' ); ?></p>
+<p class="text-p--2 xl:max-w-[320px]"><?php the_field( 'sidebar_text_2' ); ?></p>
+<div class="mt-10 md:mt-12 xl:mt-14">
+    <?php
+    $product_link = get_field('button');
+    if ($product_link):
+        $product_link_target = $product_link['target'] ? $product_link['target'] : '_self';
+        ?>
+        <a class="btn btn--secondary uppercase inline-block" href="<?php echo esc_url($product_link['url']); ?>"
+            target="<?php echo esc_attr($product_link_target); ?>"><?php echo esc_html($product_link['title']); ?></a>
+    <?php endif; ?>
+</div>
