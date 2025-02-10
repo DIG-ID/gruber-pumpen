bb67b9fbc4231347ad7a2a455e66cae6290c1b52
Daniel-Digid <daniel@dig.id>
2025-02-10 16:27:54 +0000

    d

--- template-parts/posts/flexible-steigleitung/gallery.php
+++ template-parts/posts/flexible-steigleitung/gallery.php
@@ -7,7 +7,7 @@ if ($images): ?>
             <a href="<?php echo esc_url($image['url']); ?>" data-fancybox="gallery" data-caption="<?php echo esc_attr($image['caption']); ?>">
                 <img src="<?php echo esc_url($image['sizes']['products-gallery-thumbs']); ?>" 
                      alt="<?php echo esc_attr($image['alt']); ?>" 
-                     class="h-auto object-cover rounded-lg shadow-md transition-transform transform max-w-[282px] max-h-[187px]" />
+                     class="h-auto object-cover xl:max-w-[282px] xl:max-h-[187px]" />
             </a>
         <?php endforeach; ?>
     </div>
