9b9041039340450b8beafa47889bee17671cd6e7
Daniel-Digid <daniel@dig.id>
2025-02-12 11:26:22 +0000

    d

--- template-parts/archives/product/products.php
+++ template-parts/archives/product/products.php
@@ -16,7 +16,7 @@
                     while ( $products_query->have_posts() ) :
                         $products_query->the_post();
                         ?>
-                        <div class="swiper-slide fade-in bg-white md:px-9 xl:px-7 pt-6 pb-10 md:pt-10 md:pb-32 mb-10 xl:mb-0 rounded-3xl">
+                        <div class="swiper-slide fade-in bg-white md:px-9 xl:px-7 pt-6 pb-20 md:pt-10 md:pb-32 mb-10 xl:mb-0 rounded-3xl">
                             <div>
                                 <?php
                                 $image = get_post_thumbnail_id();
@@ -27,9 +27,11 @@
                             </div>
                             <h2 class="text-title-h4 text-blue-shade-4 text-left mt-3 md:mt-6 mb-5 md:mb-6 px-4 md:px-0 fade-in"><?php the_title(); ?></h2>
                             <p class="text-p--2 px-4 md:px-0 invisible fade-in"><?php the_field( 'description' ); ?></p>
+                            <div class="px-4 md:px-0">
                             <a class="btn btn--secondary uppercase inline-block mt-10 absolute bottom-7" href="<?php the_permalink(); ?>">
                                 <?php esc_html_e('MEHR ERFAHREN', 'gruber-pumpen'); ?>
                             </a>
+                            </div>
                         </div>
                         <?php
                     endwhile;
