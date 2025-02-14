a163690b03fe48faa50d0427beb5b0206081ebe1
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-02-14 16:36:03 +0000

    adjust the order of the products single post layout

--- template-parts/posts/flexible-steigleitung/layout.php
+++ template-parts/posts/flexible-steigleitung/layout.php
@@ -1,22 +1,22 @@
 <section class="section-content pt-10 md:pt-20 xl:pt-24 pb-24 xl:pb-40 overflow-hidden bg-blue-shade-0">
     <div class="theme-container theme-grid">
-        <div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9">
+        <div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9 order-1 xl:order-1">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/content-1'); ?>
         </div>
         
-        <div id="content-2" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9">
+        <div id="content-2" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9 order-2 xl:order-2">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/content-2'); ?>
         </div>
         
-        <div id="content-3" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9">
+        <div id="content-3" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9 order-3 xl:order-3">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/content-3'); ?>
         </div>
         
-        <div id="gallery" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1">
+        <div id="gallery" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 order-5 xl:order-4">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/gallery'); ?>
         </div>
         
-        <div id="sidebar" class="col-span-2 md:col-span-6 xl:col-span-4 col-start-1 xl:col-start-9 xl:row-start-1 xl:row-span-2 pt-16 xl:pt-0">
+        <div id="sidebar" class="col-span-2 md:col-span-6 xl:col-span-4 col-start-1 xl:col-start-9 xl:row-start-1 xl:row-span-2 pt-16 xl:pt-0 order-4 xl:order-5">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/sidebar'); ?>
         </div>
     </div>
