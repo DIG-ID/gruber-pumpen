5ffb99d059f4b56c89b66a7615676e4009cf6463
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-02-14 16:42:47 +0000

    changed the order of the single products layout

--- template-parts/posts/pe-behalter/layout.php
+++ template-parts/posts/pe-behalter/layout.php
@@ -1,9 +1,9 @@
 <section class="section-content-1 pt-10 md:pt-20 xl:pt-24 pb-24 xl:pb-40 overflow-hidden bg-blue-shade-0">
 	<div class="theme-container theme-grid">
-		<div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12">
+		<div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12 order-1 xl:order-1">
 			<?php get_template_part( 'template-parts/posts/pe-behalter/content-1'); ?>
 		</div>
-		<div id="content-button" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 my-12">
+		<div id="content-button" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 my-12 order-2 xl:order-2">
 				<?php
 				$product_link = get_field( 'button' );
 				if ( $product_link ) :
@@ -13,7 +13,7 @@
 					</a>
 				<?php endif; ?>
 		</div>
-		<div id="gallery" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1">
+		<div id="gallery" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 order-3 xl:order-3">
 			<?php get_template_part( 'template-parts/posts/pe-behalter/gallery'); ?>
 		</div>
 	</div>
--- template-parts/posts/regenwassertanks/layout.php
+++ template-parts/posts/regenwassertanks/layout.php
@@ -1,18 +1,18 @@
 <section class="section-content pt-10 md:pt-20 xl:pt-24 pb-24 xl:pb-40 overflow-hidden bg-blue-shade-0">
     <div class="theme-container theme-grid">
-        <div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 theme-grid border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-28 mb-7 md:mb-9 xl:mb-14">
+        <div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 theme-grid border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-28 mb-7 md:mb-9 xl:mb-14  order-1 xl:order-1">
             <?php get_template_part( 'template-parts/posts/regenwassertanks/content-1'); ?>
         </div>
         
-        <div id="content-2" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 theme-grid border-b border-blue-shade-2 pb-12 md:pb-7 mb-7 md:mb-9 xl:mb-14">
+        <div id="content-2" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 theme-grid border-b border-blue-shade-2 pb-12 md:pb-7 mb-7 md:mb-9 xl:mb-14  order-2 xl:order-2">
             <?php get_template_part( 'template-parts/posts/regenwassertanks/content-2'); ?>
         </div>
         
-        <div id="content-3" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 theme-grid border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-16 mb-7 md:mb-9 xl:mb-16">
+        <div id="content-3" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 theme-grid border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-16 mb-7 md:mb-9 xl:mb-16  order-3 xl:order-3">
             <?php get_template_part( 'template-parts/posts/regenwassertanks/content-3'); ?>
         </div>
         
-        <div id="gallery" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1">
+        <div id="gallery" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 order-4 xl:order-4">
             <?php get_template_part( 'template-parts/posts/regenwassertanks/gallery'); ?>
         </div>
         
