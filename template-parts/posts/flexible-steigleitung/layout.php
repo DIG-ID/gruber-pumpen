ec14479e6007501b68a932f4e235fd168dbacd6e
Daniel-Digid <daniel@dig.id>
2025-02-14 16:49:29 +0000

    d

--- template-parts/posts/flexible-steigleitung/layout.php
+++ template-parts/posts/flexible-steigleitung/layout.php
@@ -12,7 +12,7 @@
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/content-3'); ?>
         </div>
         
-        <div id="gallery" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 order-5 xl:order-4">
+        <div id="gallery" class="col-span-2 md:col-span-6 xl:col-span-12 col-start-1 order-5 xl:order-4 mt-16">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/gallery'); ?>
         </div>
         
