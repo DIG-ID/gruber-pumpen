c969e4e7c4eca81f68477468b222ed2a7952b542
Daniel-Digid <daniel@dig.id>
2025-02-18 11:48:44 +0000

    d

--- post-templates/post-pe-behalter.php
+++ post-templates/post-pe-behalter.php
@@ -11,6 +11,11 @@ if ( have_posts() ) :
 		do_action( 'before_main_content' );
 		get_template_part( 'template-parts/pages/page-header' );
 		get_template_part( 'template-parts/posts/pe-behalter/layout');
+		?>
+		<div class="theme-container py-12">
+			<a class="font-outfit font-medium text-lg text-blue-shade-5" href="<?php echo esc_url( get_post_type_archive_link( 'eigenprodukte' ) ); ?>"><?php esc_html_e( '< Zurück zur Übersicht', 'gruber-pumpen' ); ?></a>
+		</div>
+		<?php
 		do_action( 'after_main_content' );
 	endwhile;
 endif;
--- template-parts/posts/flexible-steigleitung/layout.php
+++ template-parts/posts/flexible-steigleitung/layout.php
@@ -1,4 +1,4 @@
-<section class="section-content pt-10 md:pt-20 xl:pt-24 pb-24 xl:pb-40 overflow-hidden bg-blue-shade-0">
+<section class="section-content pt-10 md:pt-20 xl:pt-24 pb-0 xl:pb-0 overflow-hidden bg-blue-shade-0">
     <div class="theme-container theme-grid">
         <div id="content-1" class="col-span-2 md:col-span-6 xl:col-span-7 col-start-1 border-b border-blue-shade-2 pb-12 md:pb-7 xl:pb-12 mb-7 md:mb-9 order-1 xl:order-1">
             <?php get_template_part( 'template-parts/posts/flexible-steigleitung/content-1'); ?>
